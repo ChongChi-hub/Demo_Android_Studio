aaf53f3eb3d3256aef21bf0f372ea1b99f22b73d
Trong Tri <trongtri@Trongs-MacBook-Air.local>
2026-04-01 11:48:48 +0700

    update fragment

---
+++ Demo_Fragment/app/src/main/java/com/example/demo_fragment/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.demo_fragment;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity implements com.example.demo_fragment.FragmentB.Counter {
+
+    int count = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+//    public void incrementValue(int value){
+//        FragmentB fragmentB;
+//        fragmentB = (FragmentB) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView2);
+//        if(fragmentB != null){
+//            fragmentB.setCount(value);
+//        }
+//    }
+
+    @Override
+    public void increateValue(int count){
+        FragmentB fragmentB;
+        fragmentB = (FragmentB) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView2);
+        if(fragmentB != null){
+            fragmentB.setCount(count);
+        }
+    }
+}
